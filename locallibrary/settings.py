b985e3939a0afb5d5b0218c1d8fa5417eda7ad28
Hasib Ashari <hasibashari@gmail.com>
2025-05-20 23:42:42 +0700

    Adds models for library catalog
    Adds database models for Genre, Language, Book, BookInstance, and Author.
    
    Configures the catalog application in settings.py.
    
    Sets up basic URL routing for the catalog app and redirects the root URL to the catalog home page.
    
    Also, sets the default timezone to Europe/London.

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add our new application
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    
+    # The home page redirects to the catalog
+    path('', RedirectView.as_view(url='catalog/', permanent=True))
 ]
+
+
+# static files (only in development)
+if settings.DEBUG: 
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
